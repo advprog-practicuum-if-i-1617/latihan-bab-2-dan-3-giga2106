27c5c2bf1df2103b2402a14b690bc7c2c7eb0f2d
giga2106 <giga2106@gmail.com>
2017-03-23 11:21:41 +0700

    SWAP GIGA edittt

--- src/latihan/swapmain.java
+++ src/latihan/swapmain.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class swapmain {
     public static void main(String[] args) {
         swap bil = new swap();
-        bil.ubah(11);
-        bil.ubah("sebelas");
+        bil.ubah(19);
+        bil.ubah("sembilan belas");
     }
 }
