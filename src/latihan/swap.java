42a7c6725df358781a906efbe588a96aad2e8abd
giga2106 <giga2106@gmail.com>
2017-03-23 12:15:03 +0700

    SWAP GIGA editttTTT

--- src/latihan/swap.java
+++ src/latihan/swap.java
@@ -1,49 +1,53 @@
 package latihan;
+
 public class swap {
+
     public int angka;
     public String huruf;
+
     public void ubah(int a) {
-        String b = "", c = "", d = "a", e = "";
-        int a1, a2;
-        if (a == 1000 || a == 100 ||a==11|| a == 10 || a == 0) {
-            if (a == 1000) {
-                e = " Seribu";
-            } else if (a == 100) {
-                e = " Seratus";}
-            else if (a==11){
-                e = "Sebelas";
-            }
-             else if (a == 10) {
-                e = " Sepuluh";
-            }
-            System.out.println("Hasil : " + e);
-        }
-        else if (a>11 && a<20) {
-            int t = a%10;
-            e = satuan(t) + " belas";
-            System.out.println("Hasil : " + e);
+        String b = "", c = "", d = "", e = "";
+        int a1, a2, a3;
+        if (a == 1000) {
+            System.out.println("Hasil = seribu");
+        } else if (a == 100) {
+            System.out.println("Hasil = seratus");
+        } else if (a == 11) {
+            System.out.println("Hasil = sebelas");
+        } else if (a == 10) {
+            System.out.println("Hadil = sepuluh");
         } else {
             a1 = a / 100;
-            if (a1 == 1) {
-                b = " seratus";
-            } else if (a1 == 0) {
-                b = "";
-            }
-            else{
-                b = satuan(a1) + " ratus";
-            }
-            a1 = a / 10;
-            a1 -= (a1 / 10) * 10;
-            if (a1 == 1) {
-                b = " sepuluh";
+            if (a1 == 0) {
+            } else if (a1 == 1) {
+                b = "seratus";
             } else {
-                c = satuan(a1) + " puluh";
+                b = satuan(a1)+" ratus";
             }
-            a1 = a - ((a / 10) * 10);
-            d = satuan(a1);
-            System.out.println("Hasil : " + b + " " + c + " " + d);
+            a2 = a / 10 - ((a / 100) * 10);
+            a3 = a % 10;
+            if (a2 == 0) {
+                d = satuan(a3);
+            } else if (a2 == 1) {
+                if (a3==0) {
+                    c=" sepuluh";
+                }
+                else if (a3 == 1) {
+                    c = " sebelas";
+                }
+                else {
+                    c = satuan(a3) + " belas";
+                }
+                
+            } else {
+                    c = satuan(a2)+" puluh";
+                    d = satuan(a3);
+            }
+
+        System.out.println("Hasil = " + b + " " + c + " " + d);
         }
     }
+
     public String satuan(int b) {
         String c = "";
         if (b == 9) {
@@ -69,6 +73,7 @@ public class swap {
         }
         return c;
     }
+
     public void ubah(String a) {
         int pindah = 0;
         int bil = 0;
@@ -82,20 +87,24 @@ public class swap {
             }
         }
         for (int i = 0; i < x; i++) {
-            if (l[i].equalsIgnoreCase(null+"ratus")) {
-                bil += (100*konv(l[i-1]))-konv(l[i-1]);
-            }if (l[i].equalsIgnoreCase(null+"puluh")) {
-                bil += (10*konv(l[i-1]))-konv(l[i-1]);
-            }if (l[i].equalsIgnoreCase(null+"belas")) {
+            if (l[i].equalsIgnoreCase(null + "ratus")) {
+                bil += (100 * konv(l[i - 1])) - konv(l[i - 1]);
+            }
+            if (l[i].equalsIgnoreCase(null + "puluh")) {
+                bil += (10 * konv(l[i - 1])) - konv(l[i - 1]);
+            }
+            if (l[i].equalsIgnoreCase(null + "belas")) {
                 bil += 10;
-            }if (l[i].equalsIgnoreCase(null+"seribu")) {
+            }
+            if (l[i].equalsIgnoreCase(null + "seribu")) {
                 bil = 1000;
             } else {
                 bil += konv(l[i]);
             }
         }
-        System.out.println("Hasilnya = "+bil);
+        System.out.println("Hasilnya = " + bil);
     }
+
     public int konv(String a) {
         int b = 0;
         if (a.equalsIgnoreCase(null + "satu")) {
@@ -120,12 +129,12 @@ public class swap {
             b = 10;
         } else if (a.equalsIgnoreCase(null + "seratus")) {
             b = 100;
-        }
-        else if (a.equalsIgnoreCase(null + "sebelas")) {
+        } else if (a.equalsIgnoreCase(null + "sebelas")) {
             b = 11;
         }
         return b;
     }
+
     public int hitungSpasi(String a) {
         int x = 0;
         for (int i = 0; i < a.length(); i++) {
